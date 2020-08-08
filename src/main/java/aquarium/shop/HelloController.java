0b7d4115b81a57371b32c337607060307106fa0f
Mifou <dzigsaw@wp.pl>
2020-08-08 16:17:25 +0200

    Initial commit

---
+++ src/main/java/aquarium/shop/HelloController.java
@@ -0,0 +1,14 @@
+package aquarium.shop;
+
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@RestController
+public class HelloController {
+
+    @RequestMapping("/")
+    public String index() {
+        return "Welcome in aquarium shop!";
+    }
+
+}
