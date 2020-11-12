b348c2196d55593cec1a6813958fe597617ce256
Mifou <dzigsaw@wp.pl>
2020-11-12 22:13:27 +0100

    Created Product entity and prepared REST controller

---
+++ src/main/java/aquarium/shop/Product.java
@@ -0,0 +1,67 @@
+package aquarium.shop;
+
+
+import javax.persistence.*;
+
+import static javax.persistence.GenerationType.IDENTITY;
+
+@Entity
+@Table(name = "product")
+public class Product{
+
+    @Id
+    @GeneratedValue(strategy = IDENTITY)
+    private Integer id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false)
+    private String category;
+
+    @Column(nullable = false)
+    private Double price;
+
+    public Product(){};
+
+    public Product(String category, String name, Double price) {
+        this.name = name;
+        this.price = price;
+        this.category = category;
+    }
+
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
+    public Double getPrice() {
+        return price;
+    }
+
+    public void setPrice(Double price) {
+        this.price = price;
+    }
+
+    @Override
+    public String toString() {
+        return "Product{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", category='" + category + '\'' +
+                ", price=" + price +
+                '}';
+    }
+}
---
+++ src/main/resources/schema.sql
@@ -0,0 +1,6 @@
+CREATE TABLE Product (
+    id   INTEGER      NOT NULL AUTO_INCREMENT,
+    name VARCHAR(128) NOT NULL,
+    price FLOAT NOT NULL,
+    PRIMARY KEY (id)
+);
