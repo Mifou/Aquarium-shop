61212eed8425b8e07721e561fa0004a04ed49fdb
Karol Skupien <Karol_Skupien@epam.com>
2021-04-12 22:19:25 +0200

    UI part display product list - get product list from backend service

--- aquarium-shop-ui/src/app/product/product.component.ts
+++ aquarium-shop-ui/src/app/product/product.component.ts
@@ -1,17 +1,33 @@
-import { Component, OnInit } from '@angular/core';
+import { HttpClient} from '@angular/common/http';
+import { Component, Injectable, OnInit } from '@angular/core';
 import { PRODUCTS } from './mock-products';
+import { Product } from './product';
 
+@Injectable()
 @Component({
   selector: 'app-product',
   templateUrl: './product.component.html'
 })
 export class ProductComponent implements OnInit {
 
+  httpClient: HttpClient;
   products = PRODUCTS;
 
-  constructor() { }
+  constructor(httpClient: HttpClient) {
+      this.httpClient = httpClient;
+  }
+
+  fetchProducts() {
+    this.httpClient.get<Product[]>('http://localhost:8080/products')
+    .subscribe(
+      (response: Product[]) => {
+        this.products = response;
+      }
+    );
+  }
 
   ngOnInit() {
+    this.fetchProducts();
   }
 
 }
